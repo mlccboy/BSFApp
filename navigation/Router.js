01f26ca8c0e8a101a7e494d3d8d3ca81f14d94c8
Wei Zhang <turbozv@gmail.com>
2017-10-07 23:16:04 -0700

    Fix cache and update answer

--- navigation/Router.js
+++ navigation/Router.js
@@ -1,7 +1,6 @@
 import { createRouter } from '@expo/ex-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import LessonScreen from '../screens/LessonScreen'
 import BibleScreen from '../screens/BibleScreen'
@@ -10,7 +9,6 @@ import AudioBibleScreen from '../screens/AudioBibleScreen';
 
 export default createRouter(() => ({
   home: () => HomeScreen,
-  links: () => LinksScreen,
   lesson: () => LessonScreen,
   bible: () => BibleScreen,
   settings: () => SettingsScreen,
